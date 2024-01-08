70c485abf984dc70c6c7bd4a182b3b94ff32461d
dkawale499 <dkawale499@gmail.com>
2024-01-08 19:53:26 +0530

    abc

--- src/abc/com/DemoProgram.java
+++ src/abc/com/DemoProgram.java
@@ -3,7 +3,8 @@ public class DemoProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("hgjhg");
+   System.out.println("hgjhg");
+  
 	}
 
 }
