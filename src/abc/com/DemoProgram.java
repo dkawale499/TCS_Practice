54bf5175a06dcfcb99e75a536779afd9b44368e3
dkawale499 <dkawale499@gmail.com>
2024-01-09 16:11:06 +0530

    adeed print

--- src/abc/com/DemoProgram.java
+++ src/abc/com/DemoProgram.java
@@ -4,6 +4,7 @@ public class DemoProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("hgjhg"); 
+   System.out.println("Hello");
    
 	}
 
